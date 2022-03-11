d3f825a91dfd479da7fa99127ffad0c720e1e1ea
Uzezi Jephter <kaskidsky@gmail.com>
2022-03-10 19:37:33 -0800

    My first servlet created and deployed

---
+++ src/main/java/com/jephtersmart/FirstServlet.java
@@ -0,0 +1,15 @@
+package com.jephtersmart;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class FirstServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        java.util.Date today = new java.util.Date();
+        out.println("<html> " + "<body>" + "<h1 align=center>HF\'s Chapter1 Servlet</h1>" + "<br>" + today + "</body>" +
+                "</html>");
+    }
+
+}
